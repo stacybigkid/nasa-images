7f877ff1b6c624a91e77b43f0f6013a9d95338a1
stacy <stacy.little08@gmail.com>
2021-02-06 15:10:39 -0700

    add click_draw_circle function

--- apod.py
+++ apod.py
@@ -141,10 +141,20 @@ class WisePhoto:
         # 'EVENT_FLAG_RBUTTON', 'EVENT_FLAG_SHIFTKEY', 'EVENT_LBUTTONDBLCLK', 'EVENT_LBUTTONDOWN', 
         # 'EVENT_LBUTTONUP', 'EVENT_MBUTTONDBLCLK', 'EVENT_MBUTTONDOWN', 'EVENT_MBUTTONUP', 'EVENT_MOUSEHWHEEL', 
         # 'EVENT_MOUSEMOVE', 'EVENT_MOUSEWHEEL', 'EVENT_RBUTTONDBLCLK', 'EVENT_RBUTTONDOWN', 'EVENT_RBUTTONUP']
-    def draw_circle(self, event, x, y, flags, param):
+    def circle_from_click(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDBLCLK:
-            cv2.circle(img, (x,y), 100, (255,255,0), -1)
+            cv2.circle(self.photo, (x,y), 100, (255,255,255), -1)
 
+    def click_draw_cirle(self, image):
+        img = image
+        cv2.namedWindow('image')
+        cv2.setMouseCallback('image', self.circle_from_click)
+
+        while(1):
+            cv2.imshow('image', img)
+            if cv2.waitKey(20) & 0xFF == 27:
+                break
+        cv2.destroyAllWindows()
 
 
 
@@ -156,14 +166,8 @@ if __name__ == '__main__':
     # pts = [[24, 497], [174, 502], [219, 627], [137, 701], [21, 627]]
     # poly = apod.draw_polygon(apod.photo, pts, fill=True, color=(0, 255, 255))
 
-    img = apod.photo
-    cv2.namedWindow('image')
-    cv2.setMouseCallback('image', apod.draw_circle)
+    apod.click_draw_cirle(apod.photo)
+   
 
-    
-    while(1):
-        cv2.imshow('image',img)
-        if cv2.waitKey(20) & 0xFF == 27:
-            break
-    cv2.destroyAllWindows()
+
 
